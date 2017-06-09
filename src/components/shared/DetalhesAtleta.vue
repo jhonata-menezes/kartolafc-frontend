33b28464ec14baa7b5fad4587b68d7c9916e2f91
Jhonata Menezes <jhonatamenezes10@gmail.com>
2017-06-09 18:27:55 -0300

    adicionando chartjs

--- src/components/shared/DetalhesAtleta.vue
+++ src/components/shared/DetalhesAtleta.vue
@@ -1,22 +1,43 @@
 <template>
   <div>
-    <div class="modal">
+    <div class="modal" :class="ativo != false ? 'is-active' : ''" @click="closeModal()">
       <div class="modal-background"></div>
-      <div class="modal-card">
+      <div class="modal-card" @click.stop>
         <header class="modal-card-head">
-          <p class="modal-card-title">{{atletaMercado.apelido}}</p>
-          <button class="delete"></button>
+          <p class="modal-card-title">
+            <span class="subtitle is-5">
+              {{atletaMercado.apelido}}
+            </span>
+            <picture class="image is-32x32 is-pulled-left">
+              <img :src="atletaMercado.foto">
+            </picture>
+          </p>
+          <button class="delete" @click="closeModal()"></button>
         </header>
         <section class="modal-card-body">
-          aksjaksaks
+          <div class="media">
+            <div class="media-content">
+              <div class="content">
+                <div class="subtitle is-6">
+                  ${{atletaMercado.preco_num}} Jogos: {{atletaMercado.jogos_num}}<br/>
+                  <small v-if="atletaMercado.scout['G']">Gols: {{atletaMercado.scout['G']}}</small>
+                </div>
+              </div>
+              <div>
+                
+              </div>
+            </div>
+          </div>
         </section>
+        <!--<footer class="modal-card-foot">
+        </footer>-->
       </div>
     </div>
   </div>  
 </template>
 
 <script>
-import http from './../../axios'
+import {http} from './../../axios'
 
 export default {
   props: ['ativo', 'atletaId'],
@@ -36,19 +57,23 @@ export default {
           this.$kartolafc.status.getStatus(s => {
             this.status = s
             this.atletaHistorico = r.data
-            this.mercado.atletas.forEach(e => {
-              if (e.atleta_id == this.atletaId) {
+            m.atletas.forEach(e => {
+              if (e.atleta_id === this.atletaId) {
                 this.atletaMercado = e
               }
-            }, this);
+            }, this)
           })
         }).catch(err => { console.log(err) })
       })
+    },
+
+    closeModal: function () {
+      this.$emit('update:ativo', false)
     }
   },
 
-  created: function () {
-    this.getAtleta()
+  watch: {
+    'atletaId': 'getAtleta'
   }
 
 }
