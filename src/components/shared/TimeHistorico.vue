36069207f5cc0a94edd75ee29ac4d74ea78bbbbd
Jhonata Menezes <jhonatamenezes10@gmail.com>
2017-06-16 23:31:17 -0300

    historico do time melhorado

--- src/components/shared/TimeHistorico.vue
+++ src/components/shared/TimeHistorico.vue
@@ -6,7 +6,103 @@
           <div class="loader-request-time"></div>
         </div>
         <div v-if="ativarGrafico">
-          <chart :chartData="datasets" :options="options" :height="300" :width="400"></chart>
+          <chart :chartData="datasets" :options="options" :height="300" :width="600"></chart>
+        </div>
+        <hr class="hr">
+        <div>
+          <strong class="is-bold is-5">Mais escalado</strong><br>
+          <div v-if="atletas.maisEscalado.escalado >= 0" class="media">
+            <figure class="media-left">
+              <p class="image is-48x48">
+                <img :src="atletas.maisEscalado.atleta.foto">
+              </p>
+            </figure>
+            <div class="media-content">
+              <div class="content">
+                <p>
+                  <strong class="title is-6">{{atletas.maisEscalado.atleta.apelido}}</strong><br>
+                  <strong class="subtitle is-6">{{atletas.maisEscalado.pontuacaoMais.toFixed(2)}} Pts - Escalado {{atletas.maisEscalado.escalado}}X</strong>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <hr class="hr">
+        <div>
+          <strong class="is-bold is-5">Rendeu mais pontos</strong><br>
+          <div v-if="atletas.maisPontos.escalado >= 0" class="media">
+            <figure class="media-left">
+              <p class="image is-48x48">
+                <img :src="atletas.maisPontos.atleta.foto">
+              </p>
+            </figure>
+            <div class="media-content">
+              <div class="content">
+                <p>
+                  <strong class="title is-6">{{atletas.maisPontos.atleta.apelido}}</strong><br>
+                  <strong class="subtitle is-6">{{atletas.maisPontos.pontuacaoMais.toFixed(2)}} Pts - Escalado {{atletas.maisPontos.escalado}}X</strong>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <hr class="hr">
+        <div>
+          <strong class="is-bold is-5">Rendeu menos pontos</strong><br>
+          <div v-if="atletas.menosPontos.escalado >= 0" class="media">
+            <figure class="media-left">
+              <p class="image is-48x48">
+                <img :src="atletas.menosPontos.atleta.foto">
+              </p>
+            </figure>
+            <div class="media-content">
+              <div class="content">
+                <p>
+                  <strong class="title is-6">{{atletas.menosPontos.atleta.apelido}}</strong><br>
+                  <strong class="subtitle is-6">{{atletas.menosPontos.pontuacaoMais.toFixed(2)}} Pts - Escalado {{atletas.menosPontos.escalado}}X</strong>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <hr class="hr">
+        <div>
+          <div>
+            <p class="has-text-centered">Reveja escalações passadas</p>
+            <div class="field is-grouped is-grouped-centered">
+              <p class="control">
+                <span class="select is-small">
+                  <select v-model="rodadaSelecionada">
+                    <option v-if="rodada > 0" v-for="(time, rodada) of historico" :value="rodada">Rodada {{rodada}}</option>
+                  </select>
+                </span>
+              </p>
+            </div>
+          </div>
+          <div v-if="historico[rodadaSelecionada] && historico[rodadaSelecionada].atletas">
+            <p class="has-text-centered">
+              Pontuação Total: {{historico[rodadaSelecionada].pontos.toFixed(2)}}
+            </p>
+            <hr class="hr-atleta">
+            <div v-for="t of historico[rodadaSelecionada].atletas">
+              <article class="media">
+                <figure class="media-left">
+                  <p class="image is-16x16">
+                    <img :src="pontuados.clubes[t.clube_id].Escudos['30x30']">
+                  </p>
+                </figure>
+                <div class="media-content">
+                  <div class="content">
+                    <p>
+                      <strong>{{ t.apelido }}</strong><small> {{ pontuados.posicoes[t.posicao_id].abreviacao.charAt(0).toUpperCase() + pontuados.posicoes[t.posicao_id].abreviacao.slice(1) }}</small>
+                      <strong class="is-pulled-right is-success" >{{ t.pontos_num }}</strong>
+                    </p>
+                  </div>
+                </div>
+              </article>
+              <hr class="hr-atleta">
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -25,7 +121,15 @@ export default {
   data () {
     return {
       loader: false,
+      pontuados: {},
       historico: [],
+      rodadaSelecionada: 0,
+      atletasOcorrencia: {},
+      atletas: {
+        maisPontos: {},
+        menosPontos: {},
+        maisEscalado: {}
+      },
       grafico: {
         variacao: [],
         patrimonio: [],
@@ -42,7 +146,6 @@ export default {
           onComplete: function (t) {
             let chartInstance = this.chart
             let ctx = chartInstance.ctx
-            // ctx.strokeStyle = 'rgba(0, 0, 0, 0.9)'
             ctx.fillStyle = 'rgba(0, 0, 0, 0.9)'
             ctx.textAlign = 'center'
             ctx.textBaseline = 'bottom'
@@ -80,8 +183,8 @@ export default {
           padding: {
             left: 10,
             right: 20,
-            top: 10,
-            bottom: 10
+            top: 15,
+            bottom: 15
           }
         },
         scales: {
@@ -107,6 +210,9 @@ export default {
     getHistorico: function () {
       this.loader = true
       this.ativarGrafico = false
+      this.$kartolafc.pontuados.getPontuados(p => {
+        this.pontuados = p
+      })
       this.$kartolafc.timeHistorico(this.timeId, h => {
         this.historico = h
         this.mountGraficoTime()
@@ -117,6 +223,8 @@ export default {
     mountGraficoTime: function () {
       let time = {}
       let rodada = 0
+      let index = 0
+      this.atletasOcorrencia = new Array(300)
       for (rodada in this.historico) {
         time = this.historico[rodada]
         let variacao = 0
@@ -125,6 +233,16 @@ export default {
         } else {
           for (let t of time.atletas) {
             variacao += t.variacao_num
+            index = parseInt(t.atleta_id)
+            if (this.atletasOcorrencia[index]) {
+              this.atletasOcorrencia[index]['escalado']++
+              this.atletasOcorrencia[index]['pontuacaoMais'] += t.pontos_num
+            } else {
+              this.atletasOcorrencia[index] = []
+              this.atletasOcorrencia[index]['atleta'] = t
+              this.atletasOcorrencia[index]['escalado'] = 1
+              this.atletasOcorrencia[index]['pontuacaoMais'] = t.pontos_num
+            }
           }
         }
 
@@ -141,6 +259,30 @@ export default {
 
         this.datasets.labels[rodada - 1] = rodada
       }
+
+      let atletasTemp = this.atletasOcorrencia
+      this.atletasOcorrencia = []
+      for (let a of atletasTemp) {
+        if (!a) {
+          continue
+        }
+        this.atletasOcorrencia.push(a)
+      }
+
+      // melhores e piores
+      this.atletasOcorrencia.sort((t1, t2) => {
+        if (t1.escalado === t2.escalado) return 0
+        return t1.escalado < t2.escalado ? 1 : -1
+      })
+      this.atletas.maisEscalado = this.atletasOcorrencia.slice(0, 1)[0]
+
+      this.atletasOcorrencia.sort((t1, t2) => {
+        if (t1.pontuacaoMais === t2.pontuacaoMais) return 0
+        return t1.pontuacaoMais < t2.pontuacaoMais ? 1 : -1
+      })
+
+      this.atletas.maisPontos = this.atletasOcorrencia.slice(0, 1)[0]
+      this.atletas.menosPontos = this.atletasOcorrencia.slice(-1)[0]
       this.setData(this.grafico)
       this.ativarGrafico = true
     },
@@ -203,5 +345,13 @@ export default {
 
 .clareamento {
   opacity: 0.5;
+}
+
+.hr {
+  margin: 0.5rem 0.5rem
+}
+
+.hr-atleta {
+  margin: 0.1rem 0.1rem
 }
 </style>
