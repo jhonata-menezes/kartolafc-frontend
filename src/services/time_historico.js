2d3ed7a9709a10f1952b24037b1036ede06dbd72
Jhonata Menezes <jhonatamenezes10@gmail.com>
2017-07-04 13:40:32 -0300

    request do historico assincrono

--- src/services/time_historico.js
+++ src/services/time_historico.js
@@ -2,34 +2,37 @@ import db from './../dexie'
 import {http} from './../axios'
 import getStatus from './status'
 
-const getTimeHistoricoRecursive = (timeId, callback, rodada, historico) => {
-  if (rodada === 0) {
-    callback(historico)
-    return
-  }
-
-  db.timesHistorico.get({'time.time_id': timeId, rodada_atual: rodada}).then(r => {
+function get (timeId, rodada) {
+  return db.timesHistorico.get({'time.time_id': timeId, rodada_atual: rodada}).then(r => {
     if (r) {
-      historico[rodada] = r
-      --rodada
-      getTimeHistoricoRecursive(timeId, callback, rodada, historico)
+      return r
     } else {
-      // request para pegar historico
-      http.get('/time/id/' + timeId + '/' + rodada).then(resp => {
+      return http.get('/time/id/' + timeId + '/' + rodada).then(resp => {
         db.timesHistorico.put(resp.data)
-        historico[rodada] = resp.data
-        --rodada
-        getTimeHistoricoRecursive(timeId, callback, rodada, historico)
+        return resp.data
       })
     }
+  })
+}
+
+const getTimeHistoricoRecursive = (timeId, callback, rodada) => {
+  let iterable = []
+  for (let i = rodada; i > 0; i--) {
+    iterable.push(get(timeId, i))
+  }
+  let returnComRodada = {}
+  Promise.all(iterable).then(collection => {
+    for (let c of collection) {
+      returnComRodada[c.rodada_atual] = c
+    }
+    callback(returnComRodada)
   })
 }
 
 const getTimeHistorico = (timeId, callback) => {
   getStatus.getStatus(s => {
     let rodada = s.rodada_atual - 1
-    let historico = []
-    getTimeHistoricoRecursive(timeId, callback, rodada, historico)
+    getTimeHistoricoRecursive(timeId, callback, rodada)
   })
 }
 
